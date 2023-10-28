2827e2342f76353159a586931e728837d20a26d2
Ereduardo <eduajimenez95@gmail.com>
2023-10-28 01:12:51 -0500

    añadido slider de productos en pagina comprador

--- comprador.php
+++ comprador.php
@@ -3,20 +3,43 @@ include("conexion.php");
 session_start();
 $usuarioI = $_SESSION['username'];
 $rol = $_SESSION['rol'];
+$dontshow = 0;
+$sqlVendedores = "SELECT * FROM `productos` ORDER BY `productos`.`ID` ASC ";
+$resultSqlVendedores = $conn->query($sqlVendedores);
+if(!$resultSqlVendedores){
+	die("Error con la base de datos: " . $conn->error);
+}
+$index = 0;
+$pics = array_fill(0, 5, null);
+if($resultSqlVendedores->num_rows > 0) {
+	while($fetchData = $resultSqlVendedores->fetch_assoc()){
+		$vendedorPicPath = $fetchData['imagenProducto'];
+		$pics[$index] = $vendedorPicPath;
+		$index++;
+	}
+    
+} else {
+    $dontshow = 1;
+}
 
 // Si el usuario ha iniciado sesión, se ejecuta esto
 if (isset($usuarioI) && ($rol == 3)) {
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/logo.jpg">
     <link rel="stylesheet" href="comprador.css">
     <title>ASTRA</title>
+	
+	<style>
+	.vendedores {
+		visibility: <?php if($dontshow==1){echo "Hidden";}else{echo "visible";} ?>
+	}
+
+	</style>
 </head>
 
 <body>
@@ -37,31 +60,25 @@ if (isset($usuarioI) && ($rol == 3)) {
 	<input type="radio" name="vendedor" id="t-3" checked>
 	<input type="radio" name="vendedor" id="t-4">
 	<input type="radio" name="vendedor" id="t-5">
-	<div class="vendedores">
+	<div class="vendedores" >
 		<label class="item" for="t-1">
-			<h1><img src="img/fondouno.png" alt=""></h2>
+			<h1><img src="<?php echo $pics[0]; ?>"	 alt="" id="vendedor-1"></h2>
 		</label>
 		<label class="item" for="t-2">
-			<h1>2</h2>
+			<h1><img src="<?php echo $pics[1]; ?>"	 alt="" id="vendedor-2" ></h2>
 		</label>
 		<label class="item" for="t-3">
-			<h1>3</h2>
+			<h1><img src="<?php echo $pics[2]; ?>"	 alt="" id="vendedor-3"></h2>
 		</label>
 		<label class="item" for="t-4">
-			<h1>4</h2>
+			<h1><img src="<?php echo $pics[3]; ?>"	 alt="" id="vendedor-4"></h2>
 		</label>
 		<label class="item" for="t-5">
-			<h1>5</h2>
+			<h1><img src="<?php echo $pics[4]; ?>"	 alt="" id="vendedor-5"></h2>
 		</label>
 	</div>
 	<br/>
-	<div class="dots">
-		<label for="t-1"></label>
-		<label for="t-2"></label>
-		<label for="t-3"></label>
-		<label for="t-4"></label>
-		<label for="t-5"></label>
-	</div>
+	
 </div>
 
         </Section>
@@ -69,8 +86,10 @@ if (isset($usuarioI) && ($rol == 3)) {
     </div>
 </body>
 
+
+
 </html>
-<?php  
+<?php
 }else {
     header('location: index.html');
 }
