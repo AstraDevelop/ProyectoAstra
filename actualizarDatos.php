c6a5ba47bd72af0da93fb4ea882bb3114e27645f
ELKIN0718 <elkinrodriguez0718@gmail.com>
2023-10-28 08:58:48 -0500

    Solo actualizar datos deseados

--- actualizarDatos.php
+++ actualizarDatos.php
@@ -4,106 +4,87 @@ session_start();
 $usuarioI = $_SESSION['username'];
 $mensajeAlerta = $_SESSION['mensajeAlerta'] ?? "";
 $claseAlerta = $_SESSION['claseAlerta'] ?? "";
-unset($_SESSION['mensajeAlerta'], $_SESSION['claseAlerta']);  // Limpiar los mensajes de la sesión después de usarlos
+unset($_SESSION['mensajeAlerta'], $_SESSION['claseAlerta']);
 
-// Si el usuario ha iniciado sesión, se ejecuta esto
 if (isset($usuarioI)) {
-
     $nombre = '';
     $usuario = '';
     $correo = '';
     $rol = '';
-    
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Recuperar los datos del formulario
         $nombre = $_POST['nombre'];
         $usuarioN = $_POST['usuario'];
         $correoN = $_POST['correo'];
         $contraseña = $_POST['contraseña'];
-        $confirmarContraseña = $_POST['confirmarContraseña'];
+        $nuevaContraseña = $_POST['nuevaContraseña'] ?? '';
+        $confirmarNuevaContraseña = $_POST['confirmarNuevaContraseña'] ?? '';
         $rol = $_POST['rol'];
 
-        if (isset($_POST['rol'])) {
-            $rol = $_POST['rol'];
-    
-            // Verifica si el rol se ha seleccionado
-            if (empty($rol) || !in_array($rol, ['2', '3'])) {
-                $mensajeAlerta = "Por favor, seleccione un rol válido (Vendedor o Comprador).";
-                $claseAlerta="alerta-rojo";
-            } else {
-                // Verifica que los campos no estén vacíos
-                $req = (strlen($nombre) * strlen($usuarioN) * strlen($correoN) * strlen($contraseña) * strlen($confirmarContraseña)) or $mensajeAlerta = "No se han llenado todos los campos";
-    
-                // Verifica que las contraseñas coincidan
-                if ($contraseña != $confirmarContraseña) {
-                    $mensajeAlerta = "Las contraseñas no coinciden, Verifique";
-                    $claseAlerta="alerta-rojo";
-                } else {
-                    // Encripta la contraseña usando password_hash
-                    $contraseñaEncriptada = password_hash($contraseña, PASSWORD_BCRYPT);
-                
-                    // Verifica si el usuario ya está registrado
-                    $sqlVerificacionUsuario = "SELECT * FROM usuarios WHERE Usuario = '$usuarioN'";
-                    $resultadoVerificacionUsuario = $conn->query($sqlVerificacionUsuario);
-
-                    if ($resultadoVerificacionUsuario->num_rows > 0) {
-                        $mensajeAlerta = "El usuario ya existe, por favor Cambiar.";
-                        $claseAlerta="alerta-rojo";
-                    } else {
-                        // Verifica si el correo ya está registrado
-                        $sqlVerificacionCorreo = "SELECT * FROM usuarios WHERE CorreoElectronico = '$correoN'";
-                        $resultadoVerificacionCorreo = $conn->query($sqlVerificacionCorreo);
-    
-                        if ($resultadoVerificacionCorreo->num_rows > 0) {
-                            $mensajeAlerta = "El correo ya existe, por favor Cambiar.";
-                            $claseAlerta="alerta-rojo";
-                        } else {
-                            $updateQuery = "UPDATE usuarios SET Nombre = '$nombre', Usuario = '$usuarioN', CorreoElectronico = '$correoN', rol = '$rol' WHERE (CorreoElectronico = '$usuarioI' OR Usuario = '$usuarioI')";
-
-                            if ($conn->query($updateQuery) === TRUE) {
-                                $mensajeAlerta = "Datos actualizados con éxito.";
-                                $claseAlerta="alerta-verde";
-                                $_SESSION['username'] = $usuarioN;
-                                $usuarioI = $usuarioN;
-                            } else {
-                                $mensajeAlerta = "Error al actualizar los datos: " . $conn->error;
-                                $claseAlerta="alerta-rojo";
-                            }
-                        }
-                    }       
-                }
-            }
+        if (!empty($nuevaContraseña) && $nuevaContraseña != $confirmarNuevaContraseña) {
+            $mensajeAlerta = "La nueva contraseña y su confirmación no coinciden.";
+            $claseAlerta = "alerta-rojo";
         } else {
-            $mensajeAlerta = "Por favor, seleccione un rol válido (Vendedor o Comprador).";
-            $claseAlerta="alerta-rojo";
-        }   
+            $sqlCheckCurrentPassword = "SELECT Contraseña FROM usuarios WHERE (CorreoElectronico = '$usuarioI' OR Usuario = '$usuarioI')";
+            $resultPassword = $conn->query($sqlCheckCurrentPassword);
+            $rowPassword = $resultPassword->fetch_assoc();
+
+            if (password_verify($contraseña, $rowPassword['Contraseña'])) {
+                if (!empty($nuevaContraseña)) {
+                    $contraseñaEncriptada = password_hash($nuevaContraseña, PASSWORD_BCRYPT);
+                } else {
+                    $contraseñaEncriptada = $rowPassword['Contraseña'];
+                }
+
+                // Actualización de datos del usuario
+                $updateQuery = "UPDATE usuarios SET Nombre = '$nombre', Usuario = '$usuarioN', CorreoElectronico = '$correoN', Contraseña = '$contraseñaEncriptada', rol = '$rol' WHERE (CorreoElectronico = '$usuarioI' OR Usuario = '$usuarioI')";
+
+                if ($conn->query($updateQuery) === TRUE) {
+                    $mensajeAlerta = "Datos actualizados con éxito.";
+                    $claseAlerta = "alerta-verde";
+                    $_SESSION['username'] = $usuarioN;
+                    $usuarioI = $usuarioN;
+                } else {
+                    $mensajeAlerta = "Error al actualizar los datos: " . $conn->error;
+                    $claseAlerta = "alerta-rojo";
+                }
+            } else {
+                $mensajeAlerta = "La contraseña actual ingresada es incorrecta.";
+                $claseAlerta = "alerta-rojo";
+            }
+        }
+
         $_SESSION['mensajeAlerta'] = $mensajeAlerta;
         $_SESSION['claseAlerta'] = $claseAlerta;
-        header("Location: actualizarDatos.php"); 
-        exit;   
+        header("Location: actualizarDatos.php");
+        exit;
     }
 
-    // Realizar la consulta SQL para obtener los datos del usuario
     $query = "SELECT Nombre, Usuario, CorreoElectronico, Contraseña, rol FROM usuarios WHERE (CorreoElectronico = '$usuarioI' OR Usuario = '$usuarioI')";
-
     $result = $conn->query($query);
 
     if ($result) {
         $row = $result->fetch_assoc();
 
-        // Asignar los valores de la base de datos a las variables
         if ($row) {
             $nombre = $row['Nombre'];
             $usuario = $row['Usuario'];
             $correo = $row['CorreoElectronico'];
             $rol = $row['rol'];
         } else {
-            // Manejar el caso en que no se encuentra ningún resultado o hay un error en la consulta
             echo "No se encontraron datos o hubo un error en la consulta.";
         }
+    } else {
+        echo "Error al recuperar datos del usuario";
+    }
+} else {
+    header('location: index.html');
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -111,6 +92,7 @@ if (isset($usuarioI)) {
     <link rel="stylesheet" href="styles-I-R.css">
     <title>Datos Del Usuario</title>
 </head>
+
 <body>
     <div class="container">
         <header>
@@ -118,9 +100,7 @@ if (isset($usuarioI)) {
                 <h2 class="logo">ASTRA</h2>
             </a>
         </header>
-
-        <div class="cuadroRegistro">
-            <!-- Caja del registro -->
+        <div class="cuadroRegistro" id="cuadroRegistro">
             <div class="form-box register">
                 <a href="perfil.php">
                     <span class="icon-close">
@@ -128,30 +108,29 @@ if (isset($usuarioI)) {
                     </span>
                 </a>
                 <h2>Datos Del Usuario</h2>
-                <!-- Mostrar alerta si hay un mensaje -->
                 <?php if (!empty($mensajeAlerta)) : ?>
-                       <div id="alerta" class="<?php echo $claseAlerta; ?>"><?php echo $mensajeAlerta; ?></div>
+                <div id="alerta" class="<?php echo $claseAlerta; ?>"><?php echo $mensajeAlerta; ?></div>
                 <?php endif; ?>
                 <form action="" method="POST">
                     <div class="input-box">
                         <span class="icon">
                             <ion-icon name="person"></ion-icon>
                         </span>
-                        <input type="text" name="nombre" required value="<?php echo $nombre; ?>">
+                        <input type="text" name="nombre" placeholder="Nombre" value="<?php echo $nombre; ?>">
                         <label for="">Nombre</label>
                     </div>
                     <div class="input-box">
                         <span class="icon">
                             <ion-icon name="person"></ion-icon>
                         </span>
-                        <input type="text" name="usuario" required value="<?php echo $usuario; ?>">
+                        <input type="text" name="usuario" placeholder="Usuario" value="<?php echo $usuario; ?>">
                         <label for="">Usuario</label>
                     </div>
                     <div class="input-box">
                         <span class="icon">
                             <ion-icon name="mail"></ion-icon>
                         </span>
-                        <input type="email" name="correo" required value="<?php echo $correo; ?>">
+                        <input type="email" name="correo" placeholder="Correo" value="<?php echo $correo; ?>">
                         <label for="">Correo</label>
                     </div>
                     <div class="rol">
@@ -165,40 +144,37 @@ if (isset($usuarioI)) {
                             <input value="3" name="rol" type="radio" class="input-rol" <?php echo ($rol == '3') ? 'checked' : ''; ?>>
                             <div class="btnOpcion">
                                 <span class="span-rol">Comprador</span>
-                            </div>  
+                            </div>
                         </div>
                     </div>
-                    <p class="mensaje">Para poder guardar los datos escriba su contraseña</p>
+                    <p class="mensaje">Para poder guardar los datos escriba su contraseña actual</p>
                     <div class="input-box contraseña">
                         <span class="icon">
                             <ion-icon name="lock-closed"></ion-icon>
                         </span>
                         <input type="password" name="contraseña" required>
-                        <label for="">Contraseña</label>
+                        <label for="">Contraseña Actual</label>
+                    </div>
+                    <p class="mensaje">Si desea actualizar su contraseña, ingrésela a continuación</p>
+                    <div class="input-box contraseña">
+                        <span class="icon">
+                            <ion-icon name="lock-closed"></ion-icon>
+                        </span>
+                        <input type="password" name="nuevaContraseña" placeholder="Nueva Contraseña">
+                        <label for="">Nueva Contraseña</label>
                     </div>
                     <div class="input-box contraseña">
                         <span class="icon">
                             <ion-icon name="lock-closed"></ion-icon>
                         </span>
-                        <input type="password" name="confirmarContraseña" required>
-                        <label for="">Confirmar Contraseña</label>
+                        <input type="password" name="confirmarNuevaContraseña" placeholder="Confirmar Nueva Contraseña">
+                        <label for="">Confirmar Nueva Contraseña</label>
                     </div>
-
-                    <button type="submit" class="btn">Actualizar Datos</button>
+                    <button type="submit" class="btn" id="btn">Actualizar Datos</button>
                 </form>
             </div>
         </div>
-
-        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     </div>
 </body>
+
 </html>
-<?php
-    }else {
-    echo "Error al recuperar datos del usuario";
-    }
-} else {
-    header('location: index.html');
-}
-?>
