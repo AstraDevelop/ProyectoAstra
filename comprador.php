ae1b69fd6cf558884a3e146e11e1a75cabf6a07c
ELKIN0718 <elkinrodriguez0718@gmail.com>
2023-10-28 11:23:12 -0500

    Implementacion comprador.php y catalogo.php

--- comprador.php
+++ comprador.php
@@ -3,94 +3,78 @@ include("conexion.php");
 session_start();
 $usuarioI = $_SESSION['username'];
 $rol = $_SESSION['rol'];
-$dontshow = 0;
-$sqlVendedores = "SELECT * FROM `productos` ORDER BY `productos`.`ID` ASC ";
-$resultSqlVendedores = $conn->query($sqlVendedores);
-if(!$resultSqlVendedores){
-	die("Error con la base de datos: " . $conn->error);
-}
-$index = 0;
-$pics = array_fill(0, 5, null);
-if($resultSqlVendedores->num_rows > 0) {
-	while($fetchData = $resultSqlVendedores->fetch_assoc()){
-		$vendedorPicPath = $fetchData['imagenProducto'];
-		$pics[$index] = $vendedorPicPath;
-		$index++;
-	}
-    
-} else {
-    $dontshow = 1;
+
+if (!isset($usuarioI) || $rol != '3') {
+    header('location: index.html');
+    exit();
 }
 
-// Si el usuario ha iniciado sesión, se ejecuta esto
-if (isset($usuarioI) && ($rol == 3)) {
+$busqueda = isset($_GET['buscar']) ? $_GET['buscar'] : "";
+
+// Consulta SQL para obtener todos los vendedores y su último producto
+$sql = "SELECT u.ID AS vendedorID, u.Nombre, p.imagenProducto, p.nombreProducto
+        FROM usuarios u 
+        LEFT JOIN (
+            SELECT vendedorID, imagenProducto, nombreProducto 
+            FROM productos 
+            WHERE ID IN (
+                SELECT MAX(ID) 
+                FROM productos 
+                GROUP BY vendedorID
+            )
+        ) p ON p.vendedorID = u.ID 
+        WHERE u.Rol = '2' 
+        AND u.Nombre LIKE '%$busqueda%'";
+
+$result = $conn->query($sql);
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/logo.jpg">
-    <link rel="stylesheet" href="comprador.css">
-    <title>ASTRA</title>
-	
-	<style>
-	.vendedores {
-		visibility: <?php if($dontshow==1){echo "Hidden";}else{echo "visible";} ?>
-	}
-
-	</style>
+    <link rel="stylesheet" href="styles-I-R.css">
+    <title>Comprador - ASTRA</title>
 </head>
 
-<body>
-    <div class="inicio">
+<body id="body">
+    <div class="container" id="compradorPage">
         <header>
-            <a href="">
+            <a href="index.html">
                 <h2 class="logo">ASTRA</h2>
             </a>
-            <nav class="navigation">
-                <a href="perfil.php"><button class="btnLogin">Ver Perfil</button></a>
-                <a href="cerrarSesion.php"><button class="btnLogin cerrarSesion">CERRAR SESION</button></a>
+			<nav>
+                <!-- Barra de búsqueda -->
+                <form action="comprador.php" method="GET" id="comp-searchForm">
+                    <input type="text" name="buscar" placeholder="Buscar">
+                    <button type="submit">Buscar</button>
+                </form>
             </nav>
         </header>
-        <Section id="fondo">
-        <div class="slider">
-	<input type="radio" name="vendedor" id="t-1">
-	<input type="radio" name="vendedor" id="t-2">
-	<input type="radio" name="vendedor" id="t-3" checked>
-	<input type="radio" name="vendedor" id="t-4">
-	<input type="radio" name="vendedor" id="t-5">
-	<div class="vendedores" >
-		<label class="item" for="t-1">
-			<h1><img src="<?php echo $pics[0]; ?>"	 alt="" id="vendedor-1"></h2>
-		</label>
-		<label class="item" for="t-2">
-			<h1><img src="<?php echo $pics[1]; ?>"	 alt="" id="vendedor-2" ></h2>
-		</label>
-		<label class="item" for="t-3">
-			<h1><img src="<?php echo $pics[2]; ?>"	 alt="" id="vendedor-3"></h2>
-		</label>
-		<label class="item" for="t-4">
-			<h1><img src="<?php echo $pics[3]; ?>"	 alt="" id="vendedor-4"></h2>
-		</label>
-		<label class="item" for="t-5">
-			<h1><img src="<?php echo $pics[4]; ?>"	 alt="" id="vendedor-5"></h2>
-		</label>
-	</div>
-	<br/>
-	
-</div>
-
-        </Section>
         
+
+        <!-- Mostrar vendedores y su último producto -->
+        <div class="comp-vendedores">
+            <?php while($row = $result->fetch_assoc()): ?>
+                <div class="comp-vendedor">
+				<h3>
+					<a href="catalogo.php?vendedorID=<?php echo $row['vendedorID']; ?>">
+						<?php echo $row['Nombre']; ?>
+					</a>
+				</h3>
+                    <?php if($row['imagenProducto']): ?>
+                        <img src="<?php echo $row['imagenProducto']; ?>" alt="<?php echo $row['nombreProducto']; ?>">
+                        
+                    <?php else: ?>
+                        <p>Este vendedor aún no tiene productos.</p>
+                    <?php endif; ?>
+                </div>
+            <?php endwhile; ?>
+        </div>
     </div>
 </body>
 
-
-
 </html>
-<?php
-}else {
-    header('location: index.html');
-}
-?>
